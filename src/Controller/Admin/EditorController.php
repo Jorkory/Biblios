dcf660118d775445d4a8f8416684780b05289fc4
Joris Lamaque <jor57lam@gmail.com>
2024-05-07 13:41:44 +0200

    Update page index of editor + Add page edit

--- src/Controller/Admin/EditorController.php
+++ src/Controller/Admin/EditorController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Editor;
 use App\Form\EditorType;
+use App\Repository\EditorRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,17 +17,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class EditorController extends AbstractController
 {
     #[Route('', name: 'app_admin_editor_index')]
-    public function index(): Response
+    public function index(EditorRepository $repository): Response
     {
+        $editors = $repository->findAll();
+
         return $this->render('admin/editor/index.html.twig', [
             'controller_name' => 'EditorController',
+            'editors' => $editors,
         ]);
     }
 
+
     #[Route('/new', name: 'app_admin_editor_new')]
-    public function new(Request $request, EntityManagerInterface $manager): Response
+    #[Route('/{id}/edit', name: 'app_admin_editor_edit', methods: ['GET', 'POST'])]
+    public function new(?Editor $editor, Request $request, EntityManagerInterface $manager): Response
     {
-        $editor = new Editor();
+        $editor ??= new Editor();
         $form = $this->createForm(EditorType::class, $editor);
 
         $form->handleRequest($request);
