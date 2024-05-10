a585475353b0935eec4fb3960151aba0766f73c8
Joris Lamaque <jor57lam@gmail.com>
2024-05-10 14:07:30 +0200

    Add limit 12 per page for page public/book

--- src/Controller/Public/BookController.php
+++ src/Controller/Public/BookController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Public;
 
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,9 +15,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class BookController extends AbstractController
 {
     #[Route('', name: 'app_public_book_index', methods: ['GET'])]
-    public function index(BookRepository $repository): Response
+    public function index(Request $request, BookRepository $repository): Response
     {
-        $books = $repository->findAll();
+        $books = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new QueryAdapter($repository->findAllMaxPerPage()),
+            $request->get('page', 1),
+            12
+        );
 
         return $this->render('public/book/index.html.twig', [
             'controller_name' => 'BookController',
